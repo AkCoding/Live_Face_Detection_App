c413abc3007ed945bbcfd51449865064bdbce4a1
Arbaaz <khanarbaj45@gmail.com>
2020-11-30 09:56:43 +0530

    Feat : Detect the Face

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # startt  web cam
-cap = cv2.VideoCapture('videos/school_girl.mp4')
+cap = cv2.VideoCapture('Face.mp4')
 
 while True:
     # read image from webcam
     respose, color_img = cap.read()
 
+    # Convert to grayscale
+    gray_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
 
-
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray_img, 1.4, 7)
