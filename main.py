6d457eb2581124ff389288fd29428188d38e1457
Arbaaz <khanarbaj45@gmail.com>
2020-11-30 09:44:27 +0530

    Feat : Read Image From Web cam

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import cv2
 
 # Load the Cascade Classifier
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# startt  web cam
+cap = cv2.VideoCapture('videos/school_girl.mp4')
+
+while True:
+    # read image from webcam
+    respose, color_img = cap.read()
+
+
+
