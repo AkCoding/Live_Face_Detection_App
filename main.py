bcb0b93e3e3686dc99f1c5068996444c47db0edb
Arbaaz <khanarbaj45@gmail.com>
2020-11-30 10:09:56 +0530

    Feat : Show File

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import cv2
 # Load the Cascade Classifier
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-# startt  web cam
+# for Video File
 cap = cv2.VideoCapture('Face.mp4')
+# startt  web cam
+# cap = cv2.VideoCapture(0)
 
 while True:
     # read image from webcam
@@ -16,3 +18,16 @@ while True:
 
     # Detect the faces
     faces = face_cascade.detectMultiScale(gray_img, 1.4, 7)
+    # display rectrangle
+    for (x, y, w, h) in faces:
+        cv2.rectangle(color_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+    # display image
+    cv2.imshow('img', color_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
